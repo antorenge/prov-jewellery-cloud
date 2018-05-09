b20009624deb527c4b90395e0b3eb9354e7f28f1
Antony Oroko <antoroko@gmail.com>
2018-05-09 17:53:13 +0300

    Add products models and admin views

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,113 @@
+"""
+Model admin views for Product
+"""
 from django.contrib import admin
+from django.template.loader import render_to_string
+from .models import (Product, Component, Image, Drawing, Dimension,
+                     Material, Technique, BillOfMaterial, MaterialTechnique)
 
-# Register your models here.
+
+class DimensionInline(admin.StackedInline):
+    """Inline view for dimensions"""
+
+    model = Dimension
+    fk_name = 'product'
+    extra = 1
+    verbose_name_plural = ''
+
+
+class DrawingInline(admin.StackedInline):
+    """Inline view for drawings"""
+
+    model = Drawing
+    fk_name = 'product'
+    extra = 1
+    verbose_name_plural = ''
+
+
+class ImageInline(admin.StackedInline):
+    """Inline view for images"""
+
+    model = Image
+    fk_name = 'product'
+    extra = 1
+    verbose_name_plural = ''
+
+
+class ComponentInline(admin.StackedInline):
+    """Inline view for components"""
+
+    model = Component
+    fk_name = 'parent'
+    extra = 1
+    verbose_name_plural = ''
+
+
+class BillOfMaterialInline(admin.StackedInline):
+    """Inline view for bill of materials"""
+
+    model = BillOfMaterial
+    fk_name = 'product'
+    extra = 1
+    verbose_name_plural = ''
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """Admin view for products"""
+    list_display = ('sku', 'name', 'product_image', 'collection', 'category',
+                    'year', 'color', 'size')
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'collection', 'category', 'variance', 'year',
+                       'color', 'size', 'shape')
+        }),
+    )
+
+    inlines = (
+        ImageInline,
+        DrawingInline,
+        DimensionInline,
+        BillOfMaterialInline,
+        ComponentInline,
+    )
+
+    def product_image(self, obj):
+        """Return the image of the product"""
+        if obj.default_image:
+            return render_to_string('product_image.html', {
+                'image': obj.default_image.file,
+            })
+    product_image.short_description = 'Image'
+
+
+class MaterialTechniqueInline(admin.StackedInline):
+    """Inline view for material techniques"""
+
+    model = MaterialTechnique
+    fk_name = 'material'
+    extra = 1
+    verbose_name_plural = ''
+
+
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('name', 'units_of_measurement')
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'units_of_measurement')
+        }),
+    )
+
+    inlines = (
+        MaterialTechniqueInline,
+    )
+
+
+class TechniqueAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Technique, TechniqueAdmin)
+admin.site.register(Material, MaterialAdmin)
+admin.site.register(Product, ProductAdmin)
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,148 @@
+"""
+Models for products
+"""
 from django.db import models
+from django.apps import apps
+from django.core.validators import MinValueValidator
+from apps.shared.audit_log.models.fields import CreatingUserField
+from apps.shared.audit_log.models.fields import LastUserField
+from apps.users.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    """Products"""
+    sku = models.CharField(unique=True, verbose_name='SKU', primary_key=True,
+                           db_index=True, max_length=64, blank=True)
+    name = models.CharField(max_length=160, blank=True)
+    collection = models.CharField(blank=True, max_length=160)
+    category = models.CharField(blank=True, max_length=160)
+    year = models.PositiveIntegerField()
+    variance = models.CharField(blank=True, max_length=2)
+    color = models.CharField(blank=True, max_length=3)
+    size = models.CharField(blank=True, max_length=4)
+    shape = models.CharField(blank=True, max_length=24)
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    created_by = CreatingUserField(
+        related_name="created_by_%(app_label)s_%(class)s",
+        on_delete=models.PROTECT)
+    modified_by = LastUserField(
+        related_name='modified_by_%(app_label)s_%(class)s',
+        on_delete=models.PROTECT)
+
+    components = models.ManyToManyField('self', through='Component',
+                                        symmetrical=False,
+                                        related_name='product_components')
+
+    bill_of_materials = models.ManyToManyField('Material',
+                                               through='BillOfMaterial')
+
+    designers = models.ManyToManyField(User)
+
+    def __str__(self):
+        return "{} {}".format(self.sku, self.name)
+
+    def save(self, * args, ** kwargs):
+
+        if not self.sku:
+            product_model = apps.get_model('products', 'Product')
+            count = product_model.objects.count()
+            self.sku = str(str(self.variance) + str(self.year)[:-2] +
+                           str(self.color) + str(self.size) +
+                           str(count + 1)).upper()
+
+        super(Product, self).save(* args, ** kwargs)
+
+    @property
+    def default_image(self):
+        """Return a product default image"""
+        image_model = apps.get_model('products', 'Image')
+        return image_model.objects.filter(product=self).first()
+
+
+class Component(models.Model):
+    """Product components"""
+    parent = models.ForeignKey(Product, on_delete=models.PROTECT,
+                               related_name='parent_product')
+    child = models.ForeignKey(Product, on_delete=models.PROTECT,
+                              related_name='child_product')
+    quantity = models.FloatField(validators=[MinValueValidator(0.00), ],
+                                 default=0.00)
+
+    def __str__(self):
+        return "{} ({})".format(self.child, self.quantity)
+
+
+class Drawing(models.Model):
+    """Product drawings"""
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    file = models.ImageField(
+        upload_to='images/products/%Y/%m',
+        default='images/products/None/no-img.png')
+    label = models.CharField(max_length=160, blank=True, null=True)
+
+
+class Image(models.Model):
+    """Product images"""
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    file = models.ImageField(
+        upload_to='images/products/%Y/%m',
+        default='images/products/None/no-img.png')
+    label = models.CharField(max_length=160, blank=True, null=True)
+
+
+class Dimension(models.Model):
+    """Product dimensions and specifications"""
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    attribute = models.CharField(blank=True, max_length=160)
+    measurement = models.CharField(max_length=160, blank=True)
+
+    def __str__(self):
+        return "{} ({})".format(self.attribute, self.measurement)
+
+
+class Technique(models.Model):
+    """Techniques"""
+    name = models.CharField(blank=True, null=True, max_length=160)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class Material(models.Model):
+    """Materials"""
+    name = models.CharField(blank=True, null=True, max_length=160)
+    units_of_measurement = models.CharField(max_length=160, default=None)
+    techniques = models.ManyToManyField('Technique',
+                                        through='MaterialTechnique')
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.units_of_measurement)
+
+
+class BillOfMaterial(models.Model):
+    """Product bill of materials"""
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    quantity = models.FloatField(validators=[MinValueValidator(0.00), ],
+                                 default=0.00)
+
+    def __str__(self):
+        return "{} ({})".format(self.material, self.quantity)
+
+
+class MaterialTechnique(models.Model):
+    """Material techniques"""
+    SKILL_LEVELS = (
+        ('basic', 'Basic'),
+        ('special', 'Special'),
+        ('auxilliary', 'Auxilliary'),
+    )
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    technique = models.ForeignKey(Technique, on_delete=models.CASCADE)
+    level = models.CharField(choices=SKILL_LEVELS, default='basic',
+                             max_length=160)
+    complexity = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return "{}".format(self.technique)
