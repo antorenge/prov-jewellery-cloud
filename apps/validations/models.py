b5f80193a6c581727546ecf8a3cf5d82ffacfcfe
Antony Oroko <antoroko@gmail.com>
2018-05-10 16:03:42 +0300

    Multiple updates - Add unique_together for one to one validation relation - Update work in progress fields

--- apps/validations/models.py
+++ apps/validations/models.py
@@ -7,7 +7,7 @@ from apps.shared.audit_log.models.fields import CreatingUserField
 from apps.shared.audit_log.models.fields import LastUserField
 from apps.inventory.models import InventoryItem
 from apps.users.models import User
-from apps.purchases.models import Workshop
+from apps.purchases.models import Workshop, Product
 
 
 class Validation(models.Model):
@@ -19,12 +19,15 @@ class Validation(models.Model):
     item = models.OneToOneField(InventoryItem, on_delete=models.PROTECT)
     is_approved = models.BooleanField(default=False)
     date_validated = models.DateTimeField(auto_now=True)
-    delivered_by = models.ForeignKey(
+    validated_by = models.ForeignKey(
         User, on_delete=models.PROTECT,
-        related_name='validated_by_%(app_label)s_%(class)s')
+        related_name='validated_by_%(app_label)s_%(class)s', blank=True)
 
     stage = models.CharField(choices=PRODUCTION_STAGE,
                              default='artisan_production', max_length=160)
+
+    class Meta:
+        unique_together = ("item", "stage")
 
     def __str__(self):
         return "{} ({})".format(self.item, self.is_approved)
@@ -41,24 +44,39 @@ class WorkInProgress(models.Model):
         ('modification', 'Modification'),
         ('assembly', 'Assembly'),
     )
-
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     workshop = models.ForeignKey(Workshop, on_delete=models.PROTECT)
+    process = models.CharField(choices=PROCESS,
+                               default='artisan_production', max_length=160)
+
+    received_items = models.ManyToManyField(
+        InventoryItem, related_name='received_items_%(app_label)s_%(class)s')
     quantity_received = models.FloatField(
         validators=[MinValueValidator(0.00), ], default=0.00)
     date_received = models.DateTimeField()
+    received_from = models.ForeignKey(
+        User, related_name='received_from_%(app_label)s_%(class)s',
+        on_delete=models.PROTECT)
+
+    delivered_items = models.ManyToManyField(
+        InventoryItem, related_name='delivered_items_%(app_label)s_%(class)s',
+        blank=True)
     quantity_delivered = models.FloatField(
         validators=[MinValueValidator(0.00), ], default=0.00)
-    date_delivered = models.DateTimeField()
-    process = models.CharField(choices=PROCESS,
-                               default='artisan_production', max_length=160)
+    date_delivered = models.DateTimeField(blank=True, null=True)
+    delivered_to = models.ForeignKey(
+        User, related_name='delivered_to_%(app_label)s_%(class)s',
+        on_delete=models.PROTECT, blank=True, null=True)
+
     date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     created_by = CreatingUserField(
-        related_name="created_by_%(app_label)s_%(class)s",
+        related_name='created_by_%(app_label)s_%(class)s',
         on_delete=models.PROTECT)
     modified_by = LastUserField(
         related_name='modified_by_%(app_label)s_%(class)s',
         on_delete=models.PROTECT)
 
     def __str__(self):
-        return
+        return "{} {} ({})".format(self.product, self.quantity_received,
+                                   self.quantity_delivered)
