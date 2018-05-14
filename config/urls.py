10445ecc2876e1f49fa5b008e39b8ad362ccded6
Antony Oroko <antoroko@gmail.com>
2018-05-14 14:17:59 +0300

    Configure serving of images and files locally

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, re_path
 from rest_framework_swagger.views import get_swagger_view
@@ -25,4 +27,4 @@ urlpatterns = [
                                    namespace='products-api')),
     re_path('admin/', admin.site.urls),
     # url(r'^api-auth/', include('rest_framework.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
