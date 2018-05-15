9481da0fd335ef1dd21a847d9f6bbb9d5b03646c
Antony Oroko <antoroko@gmail.com>
2018-05-15 16:57:14 +0300

    Add validate urls

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -15,5 +15,7 @@ urlpatterns = [
     re_path(r'^', include(router.urls)),
     re_path(r'^signed/(?P<sku>.+)$',
             views.SignedProductDesignView.as_view(), name='signed_view'),
+    re_path(r'^validate$',
+            views.ValidateJWTView.as_view(), name='validate_view'),
 
 ]
