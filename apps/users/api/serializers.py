42943d66c135dfb5a39d032b4aafd421d855b4d3
Antony Oroko <antoroko@gmail.com>
2018-05-17 14:52:26 +0300

    Remove id

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -7,7 +7,8 @@ from ..models import User
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for users"""
+
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'gender', 'email', 'phone',
+        fields = ('first_name', 'last_name', 'gender', 'email', 'phone',
                   'picture', 'is_staff', 'is_active', 'date_joined')
