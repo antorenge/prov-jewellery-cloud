d78a3cb055760e4d9e9617ec6a368b973ba34ded
Antony Oroko <antoroko@gmail.com>
2018-05-18 17:07:31 +0300

    Add items field

--- apps/payments/api/serializers.py
+++ apps/payments/api/serializers.py
@@ -4,6 +4,7 @@ Payments API
 from rest_framework import serializers
 from apps.users.api.serializers import UserSerializer
 from apps.purchases.api.serializers import PurchaseOrderSerializer
+from apps.inventory.api.serializers import InventoryItemSerializer
 from ..models import OwnershipTransfer
 
 
@@ -15,9 +16,10 @@ class OwnershipTransferSerializer(serializers.ModelSerializer):
     current_owner = UserSerializer()
     created_by = UserSerializer()
     modified_by = UserSerializer()
+    items = InventoryItemSerializer()
 
     class Meta:
         model = OwnershipTransfer
-        fields = ('order', 'previous_owner', 'current_owner',
+        fields = ('order', 'previous_owner', 'current_owner', 'items',
                   'date_transferred', 'date_created', 'date_modified',
                   'created_by', 'modified_by')
