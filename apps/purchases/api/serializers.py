0944269bee6609670bc8d7ffa2593ff552fd6e11
Antony Oroko <antoroko@gmail.com>
2018-05-18 08:42:55 +0300

    Reorganize code

--- apps/purchases/api/serializers.py
+++ apps/purchases/api/serializers.py
@@ -3,9 +3,34 @@ Purchases API
 """
 from rest_framework import serializers
 from apps.users.api.serializers import UserSerializer
-from apps.products.api.serializers import MaterialSerializer
+from apps.products.api.serializers import (MaterialSerializer,
+                                           ProductDesignSerializer)
 from ..models import (ArtisanProduction, Supplier, Location,
                       PurchaseOrderProduct, PurchaseOrderDelivery)
+
+
+class PurchaseOrderProductSerializer(serializers.ModelSerializer):
+    """Serializer for purchase order products"""
+
+    product = ProductDesignSerializer()
+
+    class Meta:
+        model = PurchaseOrderProduct
+        fields = ('product', 'quantity_ordered', 'unit_price')
+
+
+class PurchaseOrderDeliverySerializer(serializers.ModelSerializer):
+    """Serializer for deliveries"""
+
+    po_product = PurchaseOrderProductSerializer()
+    delivered_by = UserSerializer()
+    received_by = UserSerializer()
+
+    class Meta:
+        model = PurchaseOrderDelivery
+        fields = ('po_product', 'quantity_delivered', 'quantity_received',
+                  'date_delivered', 'date_received', 'delivered_by',
+                  'received_by')
 
 
 class LocationSerializer(serializers.ModelSerializer):
@@ -30,6 +55,7 @@ class SupplierSerializer(serializers.ModelSerializer):
 class ArtisanProductionSerializer(serializers.ModelSerializer):
     """Serializer for artisan productions"""
 
+    po_product = PurchaseOrderProductSerializer()
     suppliers = SupplierSerializer(source='supplier_set', many=True)
     location = LocationSerializer()
     created_by = UserSerializer()
@@ -40,25 +66,3 @@ class ArtisanProductionSerializer(serializers.ModelSerializer):
         fields = ('po_product', 'quantity_produced', 'date_created',
                   'date_modified', 'created_by', 'modified_by', 'location',
                   'suppliers')
-
-
-class PurchaseOrderProductSerializer(serializers.ModelSerializer):
-    """Serializer for purchase order products"""
-
-    class Meta:
-        model = PurchaseOrderProduct
-        fields = ('product', 'quantity_ordered', 'unit_price')
-
-
-class PurchaseOrderDeliverySerializer(serializers.ModelSerializer):
-    """Serializer for deliveries"""
-
-    po_product = PurchaseOrderProductSerializer()
-    delivered_by = UserSerializer()
-    received_by = UserSerializer()
-
-    class Meta:
-        model = PurchaseOrderDelivery
-        fields = ('po_product', 'quantity_delivered', 'quantity_received',
-                  'date_delivered', 'date_received', 'delivered_by',
-                  'received_by')
