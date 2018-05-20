5be5612f85ff920f21f09dbbe45b29836b809701
Antony Oroko <antoroko@gmail.com>
2018-05-20 17:04:05 +0300

    Strip inventoryitem to show serial no only

--- apps/inventory/api/serializers.py
+++ apps/inventory/api/serializers.py
@@ -2,19 +2,12 @@
 Inventory serializers
 """
 from rest_framework import serializers
-from apps.products.api.serializers import ProductDesignSerializer
-from apps.purchases.api.serializers import PurchaseOrderDelivery
-from apps.validations.api.wip_serializer import WorkInProgressSerializer
 from ..models import InventoryItem
 
 
 class InventoryItemSerializer(serializers.ModelSerializer):
     """Serializer for inventory items"""
 
-    product = ProductDesignSerializer()
-    delivery = PurchaseOrderDelivery()
-    wip = WorkInProgressSerializer()
-
     class Meta:
         model = InventoryItem
-        fields = ('serial_no', 'product', 'delivery', 'wip',)
+        fields = ('serial_no',)
