ee79dc3e2699e8190826086905d798384b7d92c9
Antony Oroko <antoroko@gmail.com>
2018-05-20 17:06:47 +0300

    add signed validations view

--- apps/validations/api/views.py
+++ apps/validations/api/views.py
@@ -1,7 +1,9 @@
 """
 Validations API views
 """
-from rest_framework import viewsets
+import jwt
+from rest_framework import viewsets, generics
+from rest_framework.response import Response
 from ..models import Validation, WorkInProgress
 from .validation_serializer import ValidationSerializer
 from .wip_serializer import WorkInProgressSerializer
@@ -17,3 +19,17 @@ class WorkInProgressView(viewsets.ModelViewSet):
     """Return wip's"""
     queryset = WorkInProgress.objects.all()
     serializer_class = WorkInProgressSerializer
+
+
+class SignedValidationView(generics.RetrieveAPIView):
+    """Return digitally signed purchase order delivery"""
+    queryset = Validation.objects.all()
+    serializer_class = ValidationSerializer
+
+    def get_object(self, id):
+        return Validation.objects.filter(pk=id).first()
+
+    def get(self, request, id):
+        serializer = self.get_serializer(self.get_object(id))
+        encoded = jwt.encode(serializer.data, 'SECRET', algorithm='HS256')
+        return Response({'id': id, 'signed': encoded})
